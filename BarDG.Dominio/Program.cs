e11f9047cf22d602e007a384b224d05281d5d32e
emerson.fernandes <emerson.fernandes@infraclearsalecom.onmicrosoft.com>
2020-08-11 10:36:56 -0300

    indentação

--- BarDG.Dominio/Program.cs
+++ BarDG.Dominio/Program.cs
@@ -14,42 +14,21 @@ namespace BarDG.Dominio
             var itemAgua1 = new Item(3, "Agua", 70, 1);
             var itemCerveja = new Item(4, "Cerveja", 5, 5); //bug na quantidade
         
-
-
-
             var comanda = new Comanda();
-
-
 
             comanda.RegistrarItem(itemCerveja);
             comanda.RegistrarItem(itemAgua1);
             comanda.RegistrarItem(itemSuco2);
             comanda.RegistrarItem(itemSuco1);
-      
-           
-
+                 
             Console.WriteLine($"Total de itens: {comanda.Itens.Count}");
             Console.ReadLine();
 
-
-
-
-
             comanda.FechamentoComanda(itemSuco2);
-
-
-
-
 
             comanda.ResetaComanda();
 
-
-
-
             Console.WriteLine($"\nTotal, depois de limpar: {comanda.Itens.Count}");
-
-
-
         }
     }
 }
